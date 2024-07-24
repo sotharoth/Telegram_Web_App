835d97698cec531f20fdc01c470eced443bfeaf3
SotharothKuy <kuy.sot@nsllottery.com>
2024-07-24 15:27:34 +0700

    update bot

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "7359560363:AAEml-mvgAJQVbW6mY-Ryg6r6mr2JdH9UeA";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://celebrated-torte-184681.netlify.app/";
