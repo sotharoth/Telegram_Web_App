1bb6ffe7e5f258a34cfdeff962ea66f44679dce5
SotharothKuy <kuy.sot@nsllottery.com>
2024-07-24 15:48:19 +0700

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,14 +2,9 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7359560363:AAEml-mvgAJQVbW6mY-Ryg6r6mr2JdH9UeA";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('hello....'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
-    reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-    },
-  })
-);
-
+bot.start((ctx) => ctx.reply('Welcome'))
 bot.launch();
